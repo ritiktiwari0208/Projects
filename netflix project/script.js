3ddd4bd4fa900e1082de71d7f8f11a14753f1694
ritiktiwari0208 <115523281+ritiktiwari0208@users.noreply.github.com>
2024-03-19 23:17:32 +0530

    Add files via upload

---
+++ netflix project/script.js
@@ -0,0 +1,8 @@
+background: linear-gradient(130.08deg , #383528  0% , #191919 100%);
+  box-shadow: 0px 20px 40px #00000070;
+  border: 1px solid #3b3b3b;
+  padding: 10px 20px;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
